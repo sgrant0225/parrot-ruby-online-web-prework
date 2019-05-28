5006e42d9014b5eeb86c590bf49c5caf229351ca
Sharnell Grant <sharnellgrant@gmail.com>
2019-05-28 00:56:54 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
   puts "Squawk!"
   return "Squawk!"
-end
 
 def parrot (phrase) 
   puts phrase
   return phrase
 end
+
+return "Squawk!"
